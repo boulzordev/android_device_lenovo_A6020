e751350023b2d6460f918ee4d1030e991f463732
dhacker29 <dhackerdvm@gmail.com>
2015-05-31 06:44:41 -0400

    IMSEnabler: Application to toggle the Enhanced 4G LTE Mode setting
    Somewhere in the changes made by caf to IMS the AOSP ims.apk is not
    reading that Enhanced 4G LTE Mode is enabled at boot, or possibly its
    a race condition.  IMS works fine if it is toggled off and back on
    after network data is established. So we set up a reciever for network
    change events and do the toggle when mobile data is activated.
    
    Change-Id: I217d57cb298fed38ad6a3027b6ed81a5881097f9

--- device_A6020.mk
+++ device_A6020.mk
@@ -231,7 +231,9 @@ PRODUCT_PACKAGES += \
     tcpdump \
     wcnss_service
 
-
+# IMSEnabler
+PRODUCT_PACKAGES += \
+    IMSEnabler
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_A6020
