435b1726b2c5bfde1204e4e15c33701b35221836
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-21 02:57:44 +0200

    A6020: Allow vendor init to override props
    Change-Id: I788d46edc47c75b015f581ed7a60755d829b942a

--- init/init_A6020.cpp
+++ init/init_A6020.cpp
@@ -28,6 +28,8 @@
  */
 
 #include <stdlib.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 #include <stdio.h>
 
 #include "vendor_init.h"
@@ -38,6 +40,17 @@
 #define ISMATCH(a,b)    (!strncmp(a,b,PROP_VALUE_MAX))
 
 #define CMDLINE_SIZE 1024
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 void gsm_properties(bool msim);
 void set_model_config(bool plus);
