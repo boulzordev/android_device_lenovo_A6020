25d7dd9497f36f4558a3028899b24fafbe7fbb58
Shreps <shr3ps@gmail.com>
2016-06-18 23:55:46 +0200

    Change pref LCD density
    Change-Id: Ia34ef0cc1f24b448ae4da272ebf5ebd11fb94160

--- device_A6020.mk
+++ device_A6020.mk
@@ -37,6 +37,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
