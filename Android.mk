00b3d5840864a604d581edbdb99f44b76653a855
Benjamin Legrand <android@legrand.ws>
2016-06-20 12:02:37 +0200

    Add symblink to modem & co

--- Android.mk
+++ Android.mk
@@ -1,3 +1,50 @@
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),A6020)
+
 include $(CLEAR_VARS)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+
+KM_IMAGES := \
+    keymaste.b00 keymaste.b01 keymaste.b02 keymaste.b03 keymaste.mdt
+
+KM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(KM_IMAGES)))
+$(KM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Keymaster firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KM_SYMLINKS)
+
+FIRMWARE_MBA_IMAGES := \
+    mba.mbn
+
+FIRMWARE_MBA_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_MBA_IMAGES)))
+$(FIRMWARE_MBA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "MBA Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MBA_SYMLINKS)
+
+FIRMWARE_MODEM_IMAGES := \
+    modem.b00 modem.b02 modem.b04 modem.b06 modem.b09 modem.b13 \
+    modem.b15 modem.b17 modem.b21 modem.b23 modem.b26 modem.b01 \
+    modem.b03 modem.b05 modem.b08 modem.b12 modem.b14 modem.b16 \
+    modem.b18 modem.b22 modem.b25 modem.mdt 
+
+FIRMWARE_MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_MODEM_IMAGES)))
+$(FIRMWARE_MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Modem Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MODEM_SYMLINKS)
+
+
+
+endif
