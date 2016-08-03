4fd774c7da3487195742e8412a0b34b4c415e67f
Benjamin Legrand <android@legrand.ws>
2016-08-03 20:00:20 +0200

    Add a small test for parsing the cmdline

--- loader/Android.mk
+++ loader/Android.mk
@@ -6,3 +6,11 @@ LOCAL_MODULE := benloader
 LOCAL_32_BIT_ONLY := true
 
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := init.c
+LOCAL_MODULE := bencmdline
+
+LOCAL_32_BIT_ONLY := true
+
+include $(BUILD_EXECUTABLE)
