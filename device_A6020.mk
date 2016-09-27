dfc2d98cd95718da4ce5e1d95a7b77e75bb805fb
Benjamin Legrand <android@legrand.ws>
2016-09-27 20:15:01 +0200

    Do the copy in a shell script. The ro.product.variant property is correctly set but not available in the install script.

--- device_A6020.mk
+++ device_A6020.mk
@@ -131,7 +131,8 @@ PRODUCT_PACKAGES += \
 
 # Firmware patch
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/get_board_id.sh:install/bin/get_board_id.sh
+    $(LOCAL_PATH)/configs/get_board_id.sh:install/bin/get_board_id.sh \
+    $(LOCAL_PATH)/configs/move_files.sh:install/bin/move_files.sh
 
 # Media
 PRODUCT_COPY_FILES += \
