006b4349083eca684fecd4882eca325d9cffbba1
Prateek Chaubey <chaubeyprateek@gmail.com>
2017-01-16 12:22:19 +0100

    A6020: Create symlink for IMS
    Signed-off-by: chaubeyprateek <chaubeyprateek@gmail.com>
    Signed-off-by: karthick111 <karthickrko61@gmail.com>

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,5 +4,18 @@ include $(CLEAR_VARS)
 
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
+include $(CLEAR_VARS)
+
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
+
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/vendor/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+
 # include the non-open-source counterpart to this file
 -include vendor/lenovo/A6020/AndroidBoardVendor.mk
