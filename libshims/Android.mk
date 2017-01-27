c566ae58d5037496e41ebccb9311f45b8eaef689
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-01-27 21:55:55 +0100

    A6020: add shim for IMS
    This is a mix of various commits
    
    https://github.com/yu-community-os/android_vendor_volte/commit/e3f4c1e23d56600e34f46da63d1c232b77f7f422
    https://review.lineageos.org/#/c/190340/
    
    Change-Id: I595575b3f4f17fe073fb525f73e7b150fdfecd6c

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -48,3 +48,12 @@ LOCAL_SRC_FILES := rild_socket.c
 LOCAL_MODULE := rild_socket
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := MediaBuffer.cpp
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation libui libgui
+LOCAL_CFLAGS := -Wno-unused-private-field
+LOCAL_MODULE := libshims_ims
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
