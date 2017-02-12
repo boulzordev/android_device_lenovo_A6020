5404bd1e5ed3820fec46392a7d1ec08c392fd028
Daniel Stuart <daniel.stuart14@gmail.com>
2017-02-12 20:46:47 -0200

    audio: Kang from ido
    Change-Id: Ide9a875ec73dcc1acc07e9c64586213c1ce4d86f

--- device.mk
+++ device.mk
@@ -66,9 +66,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Audio config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths_qrd_skuh.xml:system/etc/mixer_paths_qrd_skuh.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_qrd_skuh.xml \
 
 # Audio
 PRODUCT_PACKAGES += \
