292e0671ee909757e00355c27670973ceac0248c
Daniel Stuart <daniel.stuart14@gmail.com>
2017-03-02 22:43:01 -0300

    A6020: Set 1080p target
    Change-Id: Ie4b152b3536d1224a3e160f3b90cc0588e3174b2

--- lineage.mk
+++ lineage.mk
@@ -18,8 +18,8 @@ $(call inherit-product, device/lenovo/A6020/full_A6020.mk)
 $(call inherit-product-if-exists, vendor/cm/config/common_full_phone.mk)
 
 # Boot animation
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_MODEL := A6020
