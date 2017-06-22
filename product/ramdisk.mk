e044a083ad747c75ffec6601698f699d01fb5bff
LegendMaster <iurimateusgto@gmail.com>
2017-06-22 09:55:37 -0300

    A6020: Switch to init.power.sh script
    * Cleaner
    * Remove init.qcom.power.rc completely
    * Based on angler/marlin init script
    
    Signed-off-by: LegendMaster <iurimateusgto@gmail.com>

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -3,7 +3,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.usb.rc \
     init.qcom.bt.sh \
-    init.qcom.power.rc \
+    init.power.sh \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom-ims.rc \
