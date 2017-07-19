0d4157a28a5008599e35a494e80bdebdf650a9cb
Vachounet <vachounet@live.fr>
2017-07-19 10:10:45 +0200

    A6020: Set double tap to wake node
    Change-Id: Ib8f3bf7497deb3730b28e9805713bac68646981d

--- board/hardware.mk
+++ board/hardware.mk
@@ -4,3 +4,7 @@ BOARD_USES_QCOM_HARDWARE := true
 # Lineage hardware
 BOARD_HARDWARE_CLASS += \
     device/lenovo/A6020/lineagehw
+
+#DoubleTapToWake support
+TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
+
