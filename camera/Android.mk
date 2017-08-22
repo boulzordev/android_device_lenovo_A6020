ab5bf78f06ba2498677b55f82758e319be452b03
Alberto97 <albertop2197@gmail.com>
2017-08-22 14:19:59 +0200

    A6020: CameraWrapper: Fix build
    Change-Id: I2f287ea15a531249477efda1980740b1b0f6764c

--- camera/Android.mk
+++ camera/Android.mk
@@ -9,7 +9,7 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libgui libutils liblog
+    libhardware liblog libcamera_client libutils libcutils libbase libgui
 
 LOCAL_SHARED_LIBRARIES += libhidltransport android.hidl.token@1.0-utils android.hardware.graphics.bufferqueue@1.0
 LOCAL_STATIC_LIBRARIES := libarect
