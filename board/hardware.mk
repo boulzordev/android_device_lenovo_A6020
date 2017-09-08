86aef8d03789f751eae7d2f843fa5d7791fdeb91
Benjamin Legrand <android@legrand.ws>
2017-09-08 21:54:27 +0200

    Revert "A6020: Drop BOARD_USES_QCOM_HARDWARE flag"
    This reverts commit 2b396bc885bf558635191adbdff5dab1494c4dbf.
    
    Change-Id: I836535c4f4ec78f5812e606b5a93a9afa3945e01

--- board/hardware.mk
+++ board/hardware.mk
@@ -5,6 +5,5 @@ BOARD_HARDWARE_CLASS += \
     hardware/cyanogen/cmhw
 
 # Qualcomm support
-# BOARD_USES_QCOM_HARDWARE := true
-
+BOARD_USES_QCOM_HARDWARE := true
 TARGET_USES_LEGACY_ANDROID_ALARM_DRIVER := true
