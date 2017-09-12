4c19421a0effb31039ba47a1f6f4db189fadb528
Shreps <shr3ps@gmail.com>
2017-09-12 10:38:53 +0200

    A6020: update camera libraries
    Change-Id: I8c002108e2a4ab4d7f032cbb6ff9b8dc83591646

--- camera/Android.mk
+++ camera/Android.mk
@@ -11,6 +11,9 @@ LOCAL_SRC_FILES := \
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils libcutils libbase libgui
 
+LOCAL_SHARED_LIBRARIES += libhidltransport android.hidl.token@1.0-utils android.hardware.graphics.bufferqueue@1.0
+LOCAL_STATIC_LIBRARIES := libarect
+
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
