740bfefea76db5fca95d2e4cd1707d25d9c7e89b
Shreps <shr3ps@gmail.com>
2017-09-12 10:45:40 +0200

    A6020: Build display HAL
    Change-Id: I400492960ebcf6707f0a516bf39d8c1db6316d87

--- product/display.mk
+++ product/display.mk
@@ -4,16 +4,20 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
     gralloc.msm8916 \
     copybit.msm8916 \
     hwcomposer.msm8916 \
     memtrack.msm8916 \
     libgenlock 
 
-
 # RenderScript HAL
 PRODUCT_PACKAGES += \
-     android.hardware.renderscript@1.0-impl
+    android.hardware.renderscript@1.0-impl
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.use_buffer_age=false
