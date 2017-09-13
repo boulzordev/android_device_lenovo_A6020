507b9e4bdd32663300f929ea390824da2c2b9e9b
Benjamin Legrand <android@legrand.ws>
2017-09-13 07:46:02 +0200

    A6020: [cameraWrapper] Bring back Wait for service
    If camera starts without the service manager ready, then lib will crash
    leaving the /dev/video* in an incorrect state.
    
    Change-Id: Ie7bf6bdf3c1a23323f8cd1d954290278319fc8bc

--- camera/Android.mk
+++ camera/Android.mk
@@ -9,7 +9,7 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libbase libgui
+    libhardware liblog libcamera_client libutils libcutils libbase libsensor
 
 LOCAL_SHARED_LIBRARIES += libhidltransport android.hidl.token@1.0-utils android.hardware.graphics.bufferqueue@1.0
 LOCAL_STATIC_LIBRARIES := libarect
