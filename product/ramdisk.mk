46c51213d88605ec0a06364c3675539756cb941e
Shreps <shr3ps@gmail.com>
2017-09-15 18:12:06 +0200

    A6020: Drop VoLTE for now
    Change-Id: Ibb759bf3a9def8cc03f833ad629fd03ff252c44c

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -6,8 +6,6 @@ PRODUCT_PACKAGES += \
     init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sh \
-    init.qcom-ims.rc \
     init.qcom.mem.sh \
     init.qcom.ril.sh \
-    init.qti.ims.sh \
     ueventd.qcom.rc
