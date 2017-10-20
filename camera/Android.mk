5ee7b6e6066f244c72157668f3ad584353154f71
dev-harsh1998 <harshitjain6751@gmail.com>
2017-10-20 11:38:25 +0530

    A6020: camera: Modernize makefile a bit
    * Remove extention include for shared libraries add them under single hood
    * Remove unneeded usage of return

--- camera/Android.mk
+++ camera/Android.mk
@@ -9,15 +9,23 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libbase libsensor
+    libhardware \
+    liblog \
+    libcamera_client \
+    libutils \
+    libcutils \
+    libbase \
+    libsensor \
+    libhidltransport \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
-LOCAL_SHARED_LIBRARIES += libhidltransport android.hidl.token@1.0-utils android.hardware.graphics.bufferqueue@1.0
-LOCAL_STATIC_LIBRARIES := libarect
+LOCAL_STATIC_LIBRARIES := \
+    libarect
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_32_BIT_ONLY := true
 
 include $(BUILD_SHARED_LIBRARY)
