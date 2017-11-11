4a77bdd98452c235c40cd0b4f27b5b6e7b5e775b
Shreps <shr3ps@gmail.com>
2017-11-11 23:35:52 +0530

    Revert "A6020: Rework ramdisk"
    This reverts commit 00fd2b6a4338f990de912dc9b9f9a16fefe74e27.
    
    Change-Id: I2d1d470e4cd13166a2b145e4b8278f8a23791ffb

--- rootdir/etc/init.power.sh
+++ rootdir/etc/init.power.sh
@@ -40,49 +40,36 @@ restorecon -R /sys/devices/system/cpu
 write /sys/devices/system/cpu/cpu4/online 1
 write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor "interactive"
 restorecon -R /sys/devices/system/cpu # must restore after interactive
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay 19000
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 99
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 20000
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay "25000 800000:50000"
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load 90
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate 40000
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq 998400
 write /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy 0
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "50 800000:80"
-write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 39000
-write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 499200
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads "1 800000:90"
+write /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time 40000
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 800000
 
 # configure governor settings for big cluster
 write /sys/devices/system/cpu/cpu0/online 1
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor "interactive"
 restorecon -R /sys/devices/system/cpu # must restore after interactive
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay "19000 1113600:49000"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay "20000 1113600:50000"
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load 85
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate 20000
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq 1113600
 write /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy 0
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "85 1113600:90 1344000:95"
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 39000
-write /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis 59000
-write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 533333
-
-# Enable thermal core_control now
-write /sys/module/msm_thermal/core_control/enabled 1
-
-# input boost configuration
-write /sys/module/cpu_boost/parameters/input_boost_freq "0:800000 4:1113600"
-write /sys/module/cpu_boost/parameters/input_boost_ms 150
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads "1 960000:85 1113600:90 1344000:80"
+write /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time 50000
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 960000
 
 # Setting b.L scheduler parameters
-write /proc/sys/kernel/sched_migration_fixup 1
 write /proc/sys/kernel/sched_upmigrate 75
 write /proc/sys/kernel/sched_downmigrate 60
-
 write /proc/sys/kernel/sched_small_task 20
 write /proc/sys/kernel/sched_mostly_idle_load 30
 write /proc/sys/kernel/sched_mostly_idle_nr_run 3
 
 write /proc/sys/kernel/sched_prefer_idle 0
-
-# android background processes are set to nice 10. Never schedule these on the big cluster.
-write /proc/sys/kernel/sched_upmigrate_min_nice 9
 
 # Enable bus-dcvs
 for cpubw in /sys/class/devfreq/cpubw*/; do
@@ -94,20 +81,28 @@ get-set-forall /sys/class/devfreq/*gpubw*/governor bw_hwmon
 get-set-forall /sys/class/devfreq/*gpubw*/bw_hwmon/io_percent 40
 get-set-forall /sys/class/devfreq/*mincpubw*/governor cpufreq
 
+# Enable all LPMs by default
+# This will enable C4, D4, D3, E4 and M3 LPMs
+write /sys/module/lpm_levels/parameters/sleep_disabled N
+
+# Enable thermal core_control now
+write /sys/module/msm_thermal/core_control/enabled 1
+
+# Enable core control
+write /sys/devices/system/cpu/cpu0/core_ctl/min_cpus 2
+write /sys/devices/system/cpu/cpu0/core_ctl/max_cpus 4
+write /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres 68
+write /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres 40
+write /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms 100
+write /sys/devices/system/cpu/cpu0/core_ctl/task_thres 4
+write /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster 1
+write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 20
+write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 5
+write /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms 5000
+write /sys/devices/system/cpu/cpu4/core_ctl/not_preferred 1
+
 # Bring up all cores online
 for cpu in /sys/devices/system/cpu/cpu*/online; do
 	write $cpu 1
 done
 restorecon -R /sys/devices/system/cpu # must restore after online
-
-# workaround for 'QCOM PowerHAL' rewriting minimum frequency on big cluster
-## revoke permissions on 'scaling_min_freq'
-chown root root /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-chmod 0444 /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-
-# Enable all LPMs by default
-# This will enable C4, D4, D3, E4 and M3 LPMs
-write /sys/module/lpm_levels/parameters/sleep_disabled N
-
-# change GPU initial power level from 400Mhz(level 2) to 220MHz(level 3) for power savings
-write /sys/class/kgsl/kgsl-3d0/default_pwrlevel 3
