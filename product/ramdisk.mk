336635e384d638a96b4d5422fc033cc5cb07f933
harry8242 <mohamedharis97@gmail.com>
2017-11-11 23:36:10 +0530

    Revert "A6020: Switch to init.power.sh script"
    This reverts commit c06ea2d8b587c7665ff592be083c297dd312fb86.

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -3,7 +3,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.usb.rc \
     init.qcom.bt.sh \
-    init.power.sh \
+    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom-ims.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -29,10 +29,11 @@ LOCAL_SRC_FILES    := etc/init.qcom.ril.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.power.sh
+LOCAL_MODULE       := init.qcom.power.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.power.sh
+LOCAL_SRC_FILES    := etc/init.qcom.power.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
