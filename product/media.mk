ff572799f7e6d543844479818cf2b3dd777f04ba
D. Andrei Măceș <dmaces@nd.edu>
2018-02-14 20:42:15 +0530

    A6020: configs: media: Create media_profiles_V1_0.xml
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Test: Compiles
    
    Bug: 36430270
    
    Change-Id: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c
    This conversion is needed for Treble OMX, which also
    requires moving the XML to TARGET_COPY_OUT_VENDOR.
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- product/media.mk
+++ product/media.mk
@@ -9,6 +9,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:system/vendor/etc/media_profiles_V1_0.xml
 
 # OMX
 PRODUCT_PACKAGES += \
