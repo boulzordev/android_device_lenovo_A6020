7ea1f2a9743b2f02f4140d6e28180d469e5580a2
Kevin Rocard <krocard@google.com>
2018-03-02 19:41:31 -0800

    A6020: Migrate to audio V4 implementation
    * This puts an end to the goddamn logspam begging for 4.0 impl
    
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7

--- product/hidl.mk
+++ product/hidl.mk
@@ -8,8 +8,8 @@ PRODUCT_PACKAGES += \
 
 #Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
 
