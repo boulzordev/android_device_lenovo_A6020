3735ed62ca4a0872420796d5cc07bbadfa05df08
harry8242 <mohamedharis97@gmail.com>
2018-03-21 00:01:52 +0530

    A6020: Add aanc tuning mixer

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -4,6 +4,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_qrd_skuh.xml \
+    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
 
 # Audio
 PRODUCT_PACKAGES += \
