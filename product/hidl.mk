d7d9fb745558db8ea50208ca957a4a45865e42d3
dianlujitao <dianlujitao@lineageos.org>
2018-03-25 17:16:17 +0800

    A6020: Switch to binderized QTI power HAL
    Change-Id: I82e8386c2f3f02f408f5ac374e6f1c486b94bb29

--- product/hidl.mk
+++ product/hidl.mk
@@ -49,7 +49,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl
+    android.hardware.power@1.0-service-qti
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
