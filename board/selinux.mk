bdb4dd033debb4d4ace4e5f18823cef2c08c4786
harry8242 <mohamedharis97@gmail.com>
2018-04-07 19:36:09 +0530

    A6020: Include qcom sepolicy

--- board/selinux.mk
+++ board/selinux.mk
@@ -1,3 +1,4 @@
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
