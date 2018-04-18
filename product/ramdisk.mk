9741905851f5decd6d4fddb9e9520149a5fbf4cb
harry8242 <mohamedharis97@gmail.com>
2018-04-18 14:44:22 +0530

    A6020: Switch to post boot script

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -3,12 +3,12 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.usb.rc \
     init.qcom.bt.sh \
-    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom-ims.rc \
     init.qcom.mem.sh \
     init.qcom.ril.sh \
+    init.qcom.post_boot.sh \
     init.qti.ims.sh \
     ueventd.qcom.rc
 
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -29,11 +29,10 @@ LOCAL_SRC_FILES    := etc/init.qcom.ril.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.power.rc
+LOCAL_MODULE       := init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.power.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES    := etc/init.qcom.post_boot.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -50,6 +49,8 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qti.ims.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
