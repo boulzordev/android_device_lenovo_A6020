861451b9779af8149ae8851542285529f81627f2
harry8242 <mohamedharis97@gmail.com>
2018-04-24 22:42:39 +0530

    A6020: post_boot: Fine tune values for a more balanced profile
    *Also reduce big cluster's max frequency

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -82,7 +82,7 @@ case "$target" in
             echo 1 > /sys/devices/system/cpu/cpu0/online
             echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
             echo "20000 800000:50000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay
-            echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
+            echo 95 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
             echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
             echo 800000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
             echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy
@@ -95,7 +95,7 @@ case "$target" in
             echo 1 > /sys/devices/system/cpu/cpu4/online
             echo "interactive" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
             echo "25000 800000:50000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay
-            echo 95 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
+            echo 90 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
             echo 40000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
             echo 998400 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
             echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy
@@ -189,22 +189,24 @@ case "$target" in
             echo 1 > /sys/devices/system/cpu/cpu0/online
             echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
             echo "19000 800000:39000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay
-            echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
+            echo 95 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
             echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
             echo 800000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
             echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy
-            echo "75 960000:85 1113600:90 1344000:95" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
+            echo "75 960000:85 1113600:90 1344000:80" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
             echo 40000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time
             echo 40000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/sampling_down_factor
             echo 400000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+            echo 1344000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+
 
             # enable governor for power cluster
             echo 1 > /sys/devices/system/cpu/cpu4/online
             echo "interactive" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
             echo 39000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay
-            echo 95 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
+            echo 90 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
             echo 20000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
-            echo 800000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
+            echo 998400 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
             echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy
             echo 90 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads
             echo 40000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time
