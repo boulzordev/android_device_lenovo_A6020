40f207fd13d3e32674113629b69d11efb12315aa
dianlujitao <dianlujitao@lineageos.org>
2018-08-27 09:18:40 +0200

    A6020: Update WCNSS configuration symlink
    * The new HAL uses /data/vendor/wifi
    
    Change-Id: I2c13fb5fe74d5d4b0e630b2bfc7152da33898784

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -33,7 +33,7 @@ LOCAL_MODULE := WCNSS_qcom_cfg.ini
 LOCAL_MODULE_CLASS := FAKE
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE): TARGET := /data/misc/wifi/$(LOCAL_MODULE)
+$(LOCAL_BUILT_MODULE): TARGET := /data/vendor/wifi/$(LOCAL_MODULE)
 $(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT)/etc/firmware/wlan/prima/$(LOCAL_MODULE)
 $(LOCAL_BUILT_MODULE):
 	$(hide) echo "Symlink: $(SYMLINK) -> $(TARGET)"
