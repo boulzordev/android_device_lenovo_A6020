5592fda6a3139b125579e0ad19c1ebbbb5f52ab7
harry8242 <mohamedharis97@gmail.com>
2018-09-15 01:23:29 +0530

    A6020: Move wifi configs to vendor

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -8,7 +8,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := hostapd_default.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/hostapd
 LOCAL_SRC_FILES    := hostapd_default.conf
 include $(BUILD_PREBUILT)
 
@@ -17,7 +17,7 @@ LOCAL_MODULE       := p2p_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/wifi
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -25,7 +25,7 @@ LOCAL_MODULE       := wpa_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/wifi
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
