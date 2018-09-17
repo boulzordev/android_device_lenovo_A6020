d34d1842e7f74be2a0850a2b6449935c06a6a118
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    A6020: Switch to 1.1 power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- product/hidl.mk
+++ product/hidl.mk
@@ -45,7 +45,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.1-service-qti
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
