4019424ffd7e3f6be9195ca9298aa6f48846b9e0
harry8242 <mohamedharis97@gmail.com>
2018-10-01 00:54:11 +0530

    A6020: kick post boot script
    * Back to good ol power script :)
    
    This reverts commit 9741905851f5decd6d4fddb9e9520149a5fbf4cb.

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -3,9 +3,9 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.usb.rc \
     init.qcom.bt.sh \
+    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.mem.sh \
-    init.qcom.post_boot.sh \
     ueventd.qcom.rc
 
