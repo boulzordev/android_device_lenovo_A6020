13bac20eb02871f6264d5bb6ccf7f0f8850903d9
harry8242 <mohamedharis97@gmail.com>
2018-12-31 17:50:46 +0530

    A6020: Kick double tap to wake

--- board/hardware.mk
+++ board/hardware.mk
@@ -3,6 +3,6 @@ BOARD_USES_QCOM_HARDWARE := true
 
 JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|device/lenovo/A6020/lineagehw|**/*.java
 
-#DoubleTapToWake support
-TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
-
+# Lineage hardware
+BOARD_HARDWARE_CLASS += \
+    device/lenovo/A6020/lineagehw
