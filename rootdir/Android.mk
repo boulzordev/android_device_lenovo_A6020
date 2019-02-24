3380ea0c9ad59ffe2dca6a29709e45867ef9aefa
harry8242 <mohamedharis97@gmail.com>
2019-02-24 21:39:11 +0530

    Revert "A6020: kick post boot script"
    This reverts commit 4019424ffd7e3f6be9195ca9298aa6f48846b9e0.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -15,11 +15,10 @@ LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.power.rc
+LOCAL_MODULE       := init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.power.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES    := etc/init.qcom.post_boot.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
