9a15b89e3464d061a2335a72bb47c4b0f94a5984
harry8242 <mohamedharis97@gmail.com>
2019-03-31 11:50:12 +0530

    A6020: include qcom common sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,8 +193,8 @@ TARGET_RIL_VARIANT := caf
 BOARD_PROVIDES_LIBRIL := false
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-#include device/qcom/sepolicy/legacy-sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
+
 #BOARD_SEPOLICY_DIRS += \
 #    device/lenovo/A6020/sepolicy
 
